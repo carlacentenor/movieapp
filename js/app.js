3cacb52508e621db3c4b6db70698640b0b066392
carlacentenor <carlacentenor@gmail.com>
2018-01-17 11:12:04 -0500

    cambios en portada

--- js/app.js
+++ js/app.js
@@ -1,20 +1,5 @@
 $(document).ready(function() {
-  // Función que obtiene la información de todas las películas Hindus que se ingresan
-  function appiCall(titleMovie) {
-    $.getJSON('http://www.omdbapi.com/?t=' + encodeURI(titleMovie) + '&apikey=1d12799f').then(function(response) {
-      $('.title-movie').text(response.Title);
-      $('.img-movie').attr('src', response.Poster);
-      $('.sinopsis-movie').text(response.Plot);
-      $('.actors-movie').text(response.Actors);
-      $('.genre-movie').text(response.Genre);
-      console.log(response);
-    });
-  }
-
-  // Llamando a la función
-  appiCall('Chennai Express');
-
   setTimeout(function() {
-    window.location.href = 'views/primera.html';
+    window.location.href = 'views/register.html';
   }, 3000);
 });
---
+++ js/search.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+  // Función que obtiene la información de todas las películas Hindus que se ingresan
+  function appiCall(titleMovie) {
+    $.getJSON('http://www.omdbapi.com/?t=' + encodeURI(titleMovie) + '&apikey=1d12799f').then(function(response) {
+      $('.title-movie').text(response.Title);
+      $('.img-movie').attr('src', response.Poster);
+      $('.sinopsis-movie').text(response.Plot);
+      $('.actors-movie').text(response.Actors);
+      $('.genre-movie').text(response.Genre);
+      console.log(response);
+    });
+  }
+
+  // Llamando a la función
+  appiCall('Chennai Express');
+});
