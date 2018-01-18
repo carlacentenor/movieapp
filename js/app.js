6e5531e1a47d787acbe269c29e62beac6156d971
YasminMattos <yasmin.mattos.conislla@gmail.com>
2018-01-18 10:09:31 -0500

    añade comentarios

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
-  setTimeout(function() {
-    window.location.href = 'views/register.html';
-  }, 3000);
+  // setTimeout(function() {
+  //   window.location.href = 'views/register.html';
+  // }, 3000);
 
   // Función que obtiene la información de todas las películas Hindus que se ingresan
   function appiCall(titleMovie) {
@@ -20,5 +20,32 @@ $(document).ready(function() {
   appiCall('krrish');
 
   // Initialize collapse button
- 
+
+  // Añade Comentarios
+  var btn = $('.btn-send');
+  var inputMsg = $('.input-msg');
+  inputMsg.on('keydown', function() {
+    btn.removeClass('hide');
+  });
+
+  btn.on('click', function() {
+    if (inputMsg.val() !== '') {
+      var msg = $('<p/>');
+      var userBox = $('<div/>');
+      var userInfo = $('<div/>');
+      var userPic = $('<img/>');
+      var userName = $('<p/>');
+      msg.text(inputMsg.val());
+      userBox.addClass('border-sha');
+      userPic.addClass('circle img-user user-login in-block');
+      userName.addClass('user-name in-block');
+      msg.appendTo(userBox);
+      userPic.appendTo(userInfo);
+      userName.appendTo(userInfo);
+      userInfo.appendTo(userBox);
+      userInfo.insertBefore(msg);
+      userBox.appendTo($('.container-comment'));
+      inputMsg.val('');
+    }
+  });
 });
